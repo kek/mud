fcc52295f868ef964c745b12e3a113b8e2c26317
Karl Eklund <localpart@gmail.com>
2012-05-14 21:23:03 +0200

    jQuery

--- public/javascripts/socket-test.js
+++ public/javascripts/socket-test.js
@@ -1,20 +1,22 @@
 var socket = io.connect(location.origin);
+
 socket.on('news', function (data) {
   console.log("Got news:");
   console.log(data);
-  output.innerHTML += "The server says: " + data.news + "\n";
+  $('#output')[0].innerHTML += "The server says: " + data.news + "\n";
 });
 
 window.onload = function () {
-  output.innerHTML += "Starting...\n";
+  $('#output')[0].innerHTML += "Starting...\n";
   socket.emit('command', {command: 'Test command' });
   console.log("Sent test command");
 
-  command_form.onsubmit = function () {
-    console.log("Sending command: " + this.command.value);
-    socket.emit('command', {command: this.command.value});
-    this.command.value = "";
-  }
-
-  command.focus();
+  $('#command')[0].focus();
 }
+
+function submitCommand () {
+  console.log("Sending command: " + $('#command')[0].value);
+  socket.emit('command', {command: $('#command')[0].value});
+  $('#command')[0].value = "";
+}
+
