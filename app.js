c7cf7350f1676c2fdd1247d8791850b0eff3b5b3
Karl Eklund <localpart@gmail.com>
2012-05-16 20:05:53 +0200

    Moved around code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express')
-,   routes = require('./routes');
+var express = require('express'),
+    routes = require('./routes');
 var app = module.exports = express.createServer();
 var io = require('socket.io').listen(app);
 
@@ -31,19 +31,5 @@ app.get('/', routes.index);
 app.listen(19000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { news: 'Connected!' });
-
-  socket.on('command', function (data) {
-    console.log("Got command:");
-    console.log(data);
-    input = data.command;
-    socket.emit('news', { news: 'You sent the command ' + input });
-    socket.broadcast.emit('news', { news: 'Someone sent the command ' + input });
-  });
-
-  // socket.on('message', function (data) {
-  //   console.log("Got message:");
-  //   console.log(data);
-  // });
-});
+var game = require("./game");
+game.start(io);
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,8 +3,15 @@ var socket = io.connect(location.origin);
 socket.on('news', function (data) {
   console.log("Got news:");
   console.log(data);
-  $('#output')[0].innerHTML += "The server says: " + data.news + "\n";
+  $('#output')[0].innerHTML += data.news + "\n";
+  window.scrollTo(0, document.body.scrollHeight);
 });
+
+function submitCommand () {
+  console.log("Sending command: " + $('#command')[0].value);
+  socket.emit('command', {command: $('#command')[0].value});
+  $('#command')[0].value = "";
+}
 
 window.onload = function () {
   $('#output')[0].innerHTML += "Starting...\n";
@@ -13,10 +20,3 @@ window.onload = function () {
 
   $('#command')[0].focus();
 }
-
-function submitCommand () {
-  console.log("Sending command: " + $('#command')[0].value);
-  socket.emit('command', {command: $('#command')[0].value});
-  $('#command')[0].value = "";
-}
-
