b6d87709c50255d02cabc410be23624533b4a7ec
Karl Eklund <localpart@gmail.com>
2012-08-09 06:37:18 +0200

    new Event class

--- game/all.js
+++ game/all.js
@@ -3,6 +3,7 @@ module.exports = function(what) {
   what.Room = require('./room.js');
   what.Player = require('./player.js');
   what.Dispatcher = require('./dispatcher.js');
+  what.Event = require('./event.js');
   what.Thing = require('./thing.js');
   what.ThingList = require('./lists.js').ThingList;
 }
